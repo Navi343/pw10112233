7c07d3414c79362896c2df2d8c7c3f4bc4b5a5d3
NancyLlamas <18170371@itculiacan.edu.mx>
2023-11-08 10:49:47 -0700

    Avance practica User Random

--- RamdomUser/randomuser.js
+++ RamdomUser/randomuser.js
@@ -1,10 +1,34 @@
+var numero = 0;
+var cantidad = 0;
+var datos = [];
+
 async function apiradomuser(){
-    const respuesta = await fetch("https://randomuser.me/api/");
-    const datos = await respuesta.json();
+    numero = 0;
+    cantidad = document.getElementById("cantidad").value;
+    const respuesta = await fetch("https://randomuser.me/api?results="+cantidad);
+    datos = await respuesta.json();
+    muestra();
+}
+function anterior(){
+    numero--;
+    if(numero < 0){
+        numero = 0;
+    }
+    muestra();
+}
+function siguiente(){
+    numero++;
+    if(numero == cantidad){
+        numero--;
+    }
+    muestra();
+}
+function muestra(){
+    console.log("Arreglo posicion "+numero);
     let foto = document.getElementById("foto");
     let nombre = document.getElementById("nombre");
     let telefono = document.getElementById("telefono");
-    foto.setAttribute("src",datos.results[0].picture.large);
-    nombre.innerHTML=datos.results[0].name.first+" "+datos.results[0].name.last;
-    telefono.innerHTML=datos.results[0].phone;
+    foto.setAttribute("src",datos.results[numero].picture.large);
+    nombre.innerHTML=datos.results[numero].name.first+" "+datos.results[0].name.last;
+    telefono.innerHTML=datos.results[numero].phone;
 }
